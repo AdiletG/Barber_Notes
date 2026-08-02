de355f25b87cca9d747a3a31a3de135ae9f7beb6
AdiletG <adiletg844@gmail.com>
2026-08-02 22:36:47 +0600

    Сделал enum для кодов ошибки, response для ответа от единого управление исключений и так же кастомный response для пагинации

---
+++ src/main/java/kg/barbernotes/barbernotes/common/dto/ErrorResponse.java
@@ -0,0 +1,32 @@
+package kg.barbernotes.barbernotes.common.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String error;
+    private String message;
+    private String path;
+    private List<ValidationError> validationErrors;
+
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ValidationError  {
+        private String field;
+        private String message;
+    }
+}
