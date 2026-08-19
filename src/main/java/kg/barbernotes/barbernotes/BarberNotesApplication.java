063cb45bd776074d9a78ccbd370b3fc5f2870d02
AdiletG <adiletg844@gmail.com>
2026-08-19 17:52:34 +0600

    Дополнил нужной аннатацией главный класс и так же yaml файл

--- src/main/java/kg/barbernotes/barbernotes/BarberNotesApplication.java
+++ src/main/java/kg/barbernotes/barbernotes/BarberNotesApplication.java
@@ -2,8 +2,10 @@ package kg.barbernotes.barbernotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BarberNotesApplication {
 
     public static void main(String[] args) {
